d6696b3f192f42907569ef4553de349e089aee91
j1nx <p.steenbergen@j1nx.nl>
2020-05-24 19:16:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -57,29 +57,8 @@ class MycroftOS(MycroftSkill):
 		self.settings_change_callback = self.on_websettings_changed
 			
 		try:
-		
-			# Handle the 'waking' visual
-			self.add_event('recognizer_loop:wakeword',
-					self.handle_listener_started)
-			self.add_event('recognizer_loop:record_end',
-					self.handle_listener_ended)
-			self.add_event('mycroft.speech.recognition.unknown',
-					self.handle_failed_stt)
-
 			# Handle Device Ready
 			self.bus.on('mycroft.ready', self.reset_screen)
-
-			# Handle the 'busy' visual
-			self.bus.on('mycroft.skill.handler.start',
-					self.on_handler_started)
-			self.bus.on('mycroft.skill.handler.complete',
-					self.on_handler_complete)
-
-			# Handle the 'speaking' visual
-			self.bus.on('recognizer_loop:audio_output_start',
-					self.on_handler_audio_start)
-			self.bus.on('recognizer_loop:audio_output_end',
-					self.on_handler_audio_end)
 
 			# Handle volume setting via PulseAudio
 			#self.add_event('mycroft.volume.set', self.on_volume_set)
@@ -153,70 +132,11 @@ class MycroftOS(MycroftSkill):
 	#	self.muted = False
 	#	call(['pactl', 'set-sink-mute', '0', '0'])
 
-	# Cleanup
-	def shutdown(self):
-		# Gotta clean up manually since not using add_event()
-		self.bus.remove('mycroft.skill.handler.start',
-				self.on_handler_started)
-		self.bus.remove('mycroft.skill.handler.complete',
-				self.on_handler_complete)
-		self.bus.remove('recognizer_loop:audio_output_start',
-				self.on_handler_audio_start)
-		self.bus.remove('recognizer_loop:audio_output_end',
-				self.on_handler_audio_end)
-
-	# Handlers
-	def on_handler_audio_start(self, message):
-		self.speaking = True
-		#framebuffer speaking visual
-		#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/speaking.png > /dev/null 2>&1')
-
-	def on_handler_audio_end(self, message):
-		self.speaking = False
-		#framebuffer background
-		#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/background.png > /dev/null 2>&1')
-
-	def on_handler_started(self, message):
-		handler = message.data.get('handler', '')
-		if self._skip_handler(handler):
-			return
-		#framebuffer thinking visual
-		#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/thinking.png > /dev/null 2>&1')
-
-	def on_handler_complete(self, message):
-		handler = message.data.get('handler', '')
-		if self._skip_handler(handler):
-			return
-
-		# If speaking has already begun, on_handler_audio_end will
-		# turn off the framebuffer
-		if not self.speaking:
-			#framebuffer background
-			#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/background.png > /dev/null 2>&1')
-
-	def _skip_handler(self, handler):
-		#Ignoring handlers from this skill
-		return any(skip in handler for skip in self.skip_list)
-
-	def handle_listener_started(self, message):
-		#framebuffer listen visual
-		#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/listen.png > /dev/null 2>&1')
-
-	def handle_listener_ended(self, message):
-		#framebuffer background
-		#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/background.png > /dev/null 2>&1')
-
-	def handle_failed_stt(self, message):
-		#framebuffer background
-		#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/background.png > /dev/null 2>&1')
-
-
 	# Device is fully started
 	def reset_screen(self, message):
 		"""Triggered after skills are initialized."""
 		self.loading = False
 		if is_paired():
-			#os.system('fbv -f -d 1 /opt/mycroft/skills/mycroftos-enclosure-skill/ui/background.png > /dev/null 2>&1')
 			self.speak_dialog('finished.booting')
 
 
